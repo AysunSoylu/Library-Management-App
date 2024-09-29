2edb0f321ccd83699dee93d42d0064e2fece548b
Aysun Soylu <aysunsoylu@Aysun-Dizustu-Bilgisayar.local>
2024-09-29 23:28:28 +0300

    vite.config.js düzeltildi

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom'] // Optimize edilecek bağımlılıkları belirtin
-  },
-  build: {
-    rollupOptions: {
-      input: '/src/main.jsx', // Giriş noktanızı belirtin
-    }
-  }
-});
-
+})
